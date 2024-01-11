4bea8e8e0e35ca5483f601fc299b6620197f7222
Al0cam <benjaminsikac@gmail.com>
2024-01-11 21:27:43 +0100

    Testing the repository in the main page

--- src/App.svelte
+++ src/App.svelte
@@ -3,17 +3,16 @@
   import FeelingPicker from "./lib/FeelingPicker.svelte";
 
   let currentDate = new Date();
-  type Feeling = {
-    name: string;
-    value: number;
-  };
 </script>
 
 <div class="bg-gray-700 min-h-screen min-w-screen">
   <main
     class="bg-gray-600 m-0 mx-auto flex flex-col justify-center w-4/6 rounded-md"
   >
-    <button class="btn btn-primary">Add team</button>
+    <button
+      class="btn btn-primary"
+      on:click={() => repository.addTeam("Test_1101-2124")}>Add team</button
+    >
     <FeelingPicker />
   </main>
 </div>
