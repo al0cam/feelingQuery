60de5b5afe257b2d1c0fd9a0db9e67471b386a3b
Al0cam <benjaminsikac@gmail.com>
2024-01-15 22:36:20 +0100

    created fields for testing of addition and setting of values

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,24 @@
 <script lang="ts">
+  import { DocumentReference } from "firebase/firestore";
   import { repository } from "./Firebase/Repository";
   import FeelingPicker from "./lib/FeelingPicker.svelte";
 
   let currentDate = new Date();
+  let teamRef: string;
 </script>
 
 <div class="bg-gray-700 min-h-screen min-w-screen">
   <main
-    class="bg-gray-600 m-0 mx-auto flex flex-col justify-center w-4/6 rounded-md"
+    class="bg-gray-600 m-0 mx-auto flex flex-col gap-2 p-2 justify-center w-4/6 rounded-md"
   >
+    <div class="flex flex-col gap-1 w-full">
+      <h1 class="text-2xl">Set team</h1>
+      <input class="input w-fill" bind:value={teamRef} />
+      <button class="btn btn-primary" on:click={() => repository.setTeamRef()}
+        >Set team</button
+      >
+    </div>
+
     <button
       class="btn btn-primary"
       on:click={() => repository.addTeam("Test_1101-2124")}>Add team</button
