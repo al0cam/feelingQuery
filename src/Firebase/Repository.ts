7d8f3947fafcb59267142156b5ad98a87c9ff93f
Al0cam <benjaminsikac@gmail.com>
2024-03-03 17:26:51 +0100

    added notification toasts to the repository that fetches values from the db

--- src/Firebase/Repository.ts
+++ src/Firebase/Repository.ts
@@ -1,6 +1,6 @@
-import { DocumentReference, addDoc, collection } from "firebase/firestore";
+import { DocumentReference, addDoc, collection, doc, getDoc } from "firebase/firestore";
 import db from "./Firebase";
-import { set } from "firebase/database";
+import { notificationStore } from "../Stores/NotificationStore";
 
 function createRepository(){
 
@@ -8,21 +8,35 @@ function createRepository(){
     let dateRef: DocumentReference;
     let feelingRef: DocumentReference;
 
+    function isEmpty(value: any, type: string){
+        if(value === undefined || value === null || value === ""){
+            notificationStore.addErrorNotification(type + " cannot be empty!");
+            return true;
+        }
+    }
 
     function addTeam(name: string){
+        if(isEmpty(name, "Team")){
+            return;
+        }
         addDoc(collection(db, "teams"), {
             name: name,
         }).then((docRef) => {
             teamRef = docRef;
             console.log("Document written with ID: ", docRef.id);
+            notificationStore.addSuccessNotification("Team added successfully");
         }
         ).catch((error) => {
+            notificationStore.addErrorNotification("Error adding team");
             console.error("Error adding document: ", error);
         });
     }
     
     // add a subcollection for the dates which will then contain feelings
     function addDate(date: Date){
+        if(isEmpty(date, "Date")){
+            return;
+        }
         addDoc(collection(teamRef, "dates"), {
             date: date
         }).then((docRef) => {
@@ -30,11 +44,15 @@ function createRepository(){
             console.log("Document written with ID: ", docRef.id);
         }
         ).catch((error) => {
+            notificationStore.addErrorNotification("Error adding date");
             console.error("Error adding document: ", error);
         });
     }
 
     function addFeeling(feeling: number){
+        if(isEmpty(feeling, "Feeling")){
+            return;
+        }
         addDoc(collection(dateRef, "feelings"), {
             feeling: feeling
         }).then((docRef) => {
@@ -42,7 +60,47 @@ function createRepository(){
             console.log("Document written with ID: ", docRef.id);
         }
         ).catch((error) => {
-            console.error("Error adding document: ", error);
+            notificationStore.addErrorNotification("Error adding feeling");
+            console.error("Error adding feeling: ", error);
+        });
+    }
+
+    function fetchTeamByName(name: string){
+        if(isEmpty(name, "Team")){
+            return;
+        }
+        teamRef = doc(db, "teams", name);
+        getDoc(teamRef).then((doc) => {
+            if (doc.exists()) {
+                console.log("Document data:", doc.data());
+                notificationStore.addSuccessNotification("Team fetched successfully");
+            } else {
+                console.log("No such document!");
+                notificationStore.addErrorNotification("Team not found");
+            }
+        })
+        .catch((error) => {
+            console.error("Error getting document:", error);
+            notificationStore.addErrorNotification("Error fetching team");
+        });
+    }
+
+    function fetchTeamDates(){
+        if(isEmpty(teamRef, "Team")){
+            return;
+        }
+        getDoc(teamRef).then((doc) => {
+            if (doc.exists()) {
+                console.log("Document data:", doc.data());
+                notificationStore.addSuccessNotification("Team fetched successfully");
+            } else {
+                console.log("No such document!");
+                notificationStore.addErrorNotification("Team not found");
+            }
+        })
+        .catch((error) => {
+            console.error("Error getting document:", error);
+            notificationStore.addErrorNotification("Error fetching team");
         });
     }
 
@@ -50,8 +108,8 @@ function createRepository(){
         addTeam,
         addDate,
         addFeeling,
-        setTeamRef: (ref: DocumentReference) => teamRef = ref,
-        setDateRef: (ref: DocumentReference) => dateRef = ref,
+        fetchTeamByName,
+        fetchTeamDates,
     }
 }
 
