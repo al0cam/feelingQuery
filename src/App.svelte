cd62d5b2bb730bd24476d600b92bf6a1aa7e8533
Al0cam <benjaminsikac@gmail.com>
2024-03-04 22:13:26 +0100

    created distinction between navbar and other components

--- src/App.svelte
+++ src/App.svelte
@@ -23,6 +23,7 @@
   <main
     class="bg-gray-600 m-0 mx-auto flex flex-col gap-2 p-2 justify-center w-4/6 rounded-md"
   >
+    <!-- Navbar -->
     <div role="tablist" class="tabs tabs-lifted">
       {#each tabs as tab, i}
         <button
@@ -35,6 +36,7 @@
         </button>
       {/each}
     </div>
+    <!-- Navbar end -->
     <span class="text-6xl mx-auto">WORK IN PROGRESS</span>
     {#if tabs[currentTab].componentName}
       <svelte:component this={tabs[currentTab].componentName} />
