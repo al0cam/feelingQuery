eaa3e513b9dd906e4ff200f471760772dbbe0c07
Al0cam <benjaminsikac@gmail.com>
2024-03-12 20:38:45 +0100

    added method for date setting

--- src/Stores/TeamStore.ts
+++ src/Stores/TeamStore.ts
@@ -1,11 +1,18 @@
 
 import { writable } from "svelte/store";
 import type { Team } from "../Models/Team";
+import type { DateModel } from "../Models/DateModel";
 
 
 
 function createStore() {
     const { subscribe, set, update } = writable<Team>();
+
+    function setDates(dates: DateModel[]) {
+        update(team => {
+            return {...team, dates: dates};
+        });
+    }
 
     return {
         subscribe,
